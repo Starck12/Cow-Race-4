79e67a862dc5188621ab6a60f83870066cc1f5de
Starck12 <daniel@tipsliga.dk>
2022-06-14 21:52:43 +0200

    Update MoveCow.cs

--- Assets/Scripts/MoveCow.cs
+++ Assets/Scripts/MoveCow.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MoveCow : MonoBehaviour
 {
-    private float speed = 1f;
+    private float speed;
     // Update is called once per frame
     void Update()
     {
+        float min = 0.2f;
+        float max = 5f;
+        speed = Random.Range(min, max) + 1;
         transform.position += transform.right * Time.deltaTime * speed;
     }
 }
